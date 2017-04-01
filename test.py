f8dca16632d87e561b475c0aec71c53bcde7071e
Gergely Imreh <imrehg@gmail.com>
2017-04-01 12:07:03 +0100

    flask example

--- test.py
+++ test.py
@@ -1,10 +1,15 @@
 import dbus
 from time import sleep
+from flask import Flask
 
 sys_bus = dbus.SystemBus()
 print(sys_bus)
 
+## Dbus
+
 def check():
+    """Avahi Server Version Check
+    """
     sys_bus = dbus.SystemBus()
     try:
         raw_server = sys_bus.get_object('org.freedesktop.Avahi', '/')
@@ -15,6 +20,8 @@ def check():
     return False
 
 def run():
+    """Announce server
+    """
     raw_server = sys_bus.get_object('org.freedesktop.Avahi', '/')
     server = dbus.Interface(raw_server, 'org.freedesktop.Avahi.Server')
     path = server.EntryGroupNew()
@@ -28,18 +35,23 @@ def run():
     result = server2.AddService(dbus.Int32(-1), # avahi.IF_UNSPEC
                                 dbus.Int32(-1), # avahi.PROTO_UNSPEC
                                 dbus.UInt32(0), # flags
-                                "webServer", # sname
+                                "Test Server", # sname
                                 "_http._tcp", # stype
                                 "local", # sdomain
-                                "0536ffe-0536ffe.local", # shost - should fill out programmically
-                                dbus.UInt16(8080), # port
-                                dbus.Array(signature="aay"))
+                                "hydra.local", # shost - should fill out programmically
+                                dbus.UInt16(8000), # port
+                                dbus.Array(signature="aay")) # TXT field, this is empty
     server2.Commit()
     print(server2.GetState()) # should be 1, as now up
+
+## Web server
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World in collaboration with Avahi and DBUS!'
 
 if __name__ == "__main__":
     check()
     run()
-    while True:
-        print("Running Server")
-        sleep(60)
+    app.run(host='0.0.0.0', port=8000)
